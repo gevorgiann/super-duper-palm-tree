d5d4067217750211ebdef7dd62aa2453c6c373e1
maham912 <maham.marashizadeh.731@my.csun.edu>
2022-01-22 22:37:54 -0800

    completed merge of python and jade

--- app.py
+++ app.py
@@ -10,25 +10,32 @@ class Building:
         self.floors = 1
         self.ada_comp = False
         self.building_name = ""
-        self.building_rating = 0
+        self.building_rating = [] 
+# error handle for zero items on the average rating
+#make calc avergae function
+
+# tuple for (rating, review)
+# when displaying tuple above, sort rating from best to worst and present some of the best and some of the worst 
+# add button to view all reviews 
+# hard code a lot of ratings into array
 
 building1 = Building()
 building1.floors = 2
 building1.ada_comp = True
 building1.building_name = "Sol Center"
-building1.building_rating = 0
+building1.building_rating 
 
 building2 = Building()
 building2.floors = 1
 building2.ada_comp = True
 building2.building_name = "Oasis"
-building2.building_rating = 0
+building2.building_rating 
 
 building3 = Building()
 building3.floors = 3
 building3.ada_comp = True
 building3.building_name = "SRC"
-building3.building_rating = 0
+building3.building_rating 
 
 db.append(building1)
 db.append(building2)
@@ -44,9 +51,9 @@ def buildings():
 
 @app.route("/ratings")
 def ratings():
-    building_number = ""
+    building_info = ""
     for building in db:
-        building_number += f"<p>{building.building_rating}</p>"
+        building_info += f"<p>{building.building_rating}</p>"
 
     return render_template('ratings.html', buildings = db)
 
@@ -57,7 +64,12 @@ def building(building_name):
         if building.building_name == building_name:
             return render_template('building.html', building = building)
     return redirect(url_for('buildings'))
+    
+@app.route("/index")
+def jade():
+    building_info = ""
+    return render_template('index.html')
 
 @app.route("/")
-def hello_world():
+def bottom():
     return render_template('base.html')
---
+++ static/main.js
@@ -0,0 +1,127 @@
+const apiKey ='pk.eyJ1IjoianNsZWltYW4iLCJhIjoiY2t5cWd0bjBsMGpuMDJubzd4aGc4ZHdwciJ9.UPLSgF0sRx8FezL36QqwSQ';
+
+const mymap = L.map('map').setView([34.241974, -118.526484], 17);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: apiKey
+}).addTo(mymap);
+
+// Adding Sol Marker
+
+const sol_marker = L.marker([34.240038, -118.527068]).addTo(mymap);
+
+// Add popup message to marker
+
+let template = `
+<h3>Sol Center</h3>
+<div style="text-align:center">
+    <img width="150" height="150"src="static/sol_center.jpg"/>
+</div>
+`
+sol_marker.bindPopup(template);
+
+// Sol center outline 
+
+const polygon = L.polygon([
+   
+    [34.240249, -118.527229],
+    [34.239839, -118.527232],
+    [34.239836, -118.527015],
+    [34.239952, -118.527023],
+    [34.239954, -118.526956],
+    [34.240056, -118.526795],
+    [34.240251, -118.5268],
+    [34.240249, -118.527229],
+], {
+    color:'red',
+    fillColor:'red',
+    fillOpacity:0.2
+}).addTo(mymap).bindPopup('Sol Center')
+
+
+// Adding Oasis Marker
+
+const Oasis_marker = L.marker([34.239318, -118.525974]).addTo(mymap); 
+
+// Add popup message to marker
+
+let template2 = `
+<h3>The Oasis</h3>
+<div style="text-align:center">
+    <img width="150" height="150"src="static/outside_oasis.jpg"/>
+</div>
+`
+Oasis_marker.bindPopup(template2);
+
+// Oasis outline 
+
+const polygon2 = L.polygon([
+   
+    [34.239535, -118.526374],
+    [34.239249, -118.526403],
+    [34.239273, -118.525639],
+    [34.239397, -118.525644],
+    [34.2394, -118.525918],
+    [34.239533, -118.525926],
+    [34.239537, -118.526379],
+
+], {
+    color:'blue',
+    fillColor:'blue',
+    fillOpacity:0.2
+}).addTo(mymap).bindPopup('The Oasis')
+
+// Adding SRC Marker
+
+const src_marker = L.marker([34.24002, -118.524923]).addTo(mymap); 
+
+// Add popup message to marker
+
+let template3 = `
+<h3>The Student Recreation Center</h3>
+<div style="text-align:center">
+    <img width="150" height="150"src="static/outside_src.png"/>
+</div>
+`
+src_marker.bindPopup(template3);
+
+// SRC outline 
+
+const polygon3 = L.polygon([
+   
+    [34.239433, -118.525169],
+    [34.239433, -118.525113],
+    [34.239329, -118.525113],
+    [34.239324, -118.524708],
+    [34.239433, -118.524705],
+    [34.239431, -118.52466],
+    [34.240491, -118.52466],
+    [34.240495, -118.524711],
+    [34.240597, -118.524713],
+    [34.240601, -118.525068],
+    [34.240621, -118.525073],
+    [34.240621, -118.525137],
+    [34.240493, -118.525137],
+    [34.240495, -118.525172],
+    [34.239431, -118.525164],
+
+
+], {
+    color:'green',
+    fillColor:'green',
+    fillOpacity:0.2
+}).addTo(mymap).bindPopup('The Student Recreation Center')
+
+
+/* Add circle 
+
+const circle = L.circle([34.240038, -118.527068], {
+    radius:25, 
+    color: 'red', 
+    fillColor: 'red',
+    fillOpacity:0.2
+}).addTo(mymap).bindPopup('Sol Center') */
